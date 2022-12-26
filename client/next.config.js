39a810bc0391562b719654800dcc489325d5a7e1
rkdwoals159 <rkdwoals159@gmail.com>
2022-12-27 01:16:09 +0900

    feat : tw macro설치 및 메타데이터 추가

--- client/next.config.js
+++ client/next.config.js
@@ -2,9 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  compiler: {
-    emotion: true
-  }
+  compiler: {}
 }
 
 module.exports = nextConfig
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,9 +1,28 @@
-import styled from '@emotion/styled'
-export default function Home() {
-  return <StyledDiv>안녕하세요 프론트엔드 개발자 강재민입니다.</StyledDiv>
+import type { NextPage } from 'next'
+import styled from '@emotion/styled/macro'
+import tw from 'twin.macro'
+
+const Input = tw.input`
+    text-center border h-28
+`
+
+const MyDiv = styled.div`
+  background: gold;
+  font-size: 5rem;
+  margin-top: 10px;
+`
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <main>
+        <h1>
+          <Input placeholder="box" />
+          <MyDiv>Test Text</MyDiv>
+        </h1>
+      </main>
+    </div>
+  )
 }
 
-const StyledDiv = styled.div`
-  /* height: 50px; */
-  /* / font-size: 200px; */
-`
+export default Home
