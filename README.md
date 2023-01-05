c3ba1214c457d60a4a099cac18f1cce7ed982021
rkdwoals159 <rkdwoals159@gmail.com>
2023-01-05 13:56:26 +0900

    feat : mongoDB 초기세팅

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # my-blog-with-vercel
 
 프론트엔드 개발자 강재민 개발블로그입니다.
+
+server env
+# port number
+PORT=8080
+# MongoDB URI
+# MONGO_URI=mongodb://localhost:27017/<db-name>
+MONGO_URI=mongodb://localhost:27017/todos
--- server/app.js
+++ server/app.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
 const createError = require("http-errors");
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
@@ -7,7 +9,14 @@ const api = require("./routes");
 const indexRouter = require("./routes/index");
 
 const app = express();
+const { PORT, MONGO_URI } = process.env;
 const { swaggerUi, specs } = require("./swagger/swagger");
+
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Successfully connected to mongodb"))
+  .catch((e) => console.error(e));
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
